b4992117bc192357b955ce07f31628ec66f52c13
ptutak <p.tutak@outlook.com>
2017-08-27 12:25:52 +0200

    transmit - ok, receive - not ok

--- main.c
+++ main.c
@@ -69,16 +69,15 @@ CommPackage dequeue(CommQueue* queue){
 
 void usartTransmit(uint8_t data) {
 /* Wait for empty transmit buffer */
-	while (!(UCSR0A & (1<<UDRE0))){};
-
+	while (!(UCSR0A & (1<<UDRE0)));
 	UDR0 = data;
 }
 
 uint8_t usartReceive(void){
 /* Wait for data to be received */
-	while (!(UCSR0A & (1<<RXC0)));
-
+	while (!(UCSR0A & (1<<RXC0))){};
 	return UDR0;
+
 }
 
 void usartInit(uint16_t baud){
@@ -90,70 +89,97 @@ void usartInit(uint16_t baud){
 /*Enable receiver and transmitter and interrupts*/
 	UCSR0B = (1<<RXEN0)|(1<<TXEN0)|(1<<TXCIE0)|(1<<RXCIE0);
 
-/* Set frame format: 8data, 2stop bit */
-	UCSR0C = (1<<USBS0)|(3<<UCSZ00);
+/* Set frame format: 8data, 1stop bit */
+	UCSR0C = (1<<UCSZ00)|(1<<UCSZ01);
 }
 
 ISR(USART_TX_vect){
 	static bool completedTransmission=true;
 	static uint16_t marker;
-	if (!usartToSendQueue()->isEmpty)
-		ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
-			CommPackage toSend=usartToSendQueue()->head->package;
-			if (!completedTransmission){
-				usartTransmit(*(toSend.package+marker));
-				marker++;
-				if (marker>toSend.size){
-					completedTransmission=true;
-					toSend=dequeue(usartToSendQueue());
-					if (toSend.dynamic)
-						free(toSend.package);
-				}
+	if (!usartToSendQueue()->isEmpty){
+	    CommPackage toSend=usartToSendQueue()->head->package;
+		if (!completedTransmission){
+            
+		    ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
+    		usartTransmit(*(toSend.package+marker));
+            marker++;
+            if (marker==toSend.size){
+				completedTransmission=true;
+				toSend=dequeue(usartToSendQueue());
+				if (toSend.dynamic)
+					free(toSend.package);
 			}
-			else{
-				usartTransmit(toSend.size);
-				marker=0;
-			}
+            }            
 		}
+		else{
+            completedTransmission=false;
+    		marker=0;
+			usartTransmit(toSend.size);
+		}
+    }
 }
 
 ISR(USART_RX_vect){
 	static bool completedTransmission=true;
 	static uint16_t marker;
-	ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
-		CommPackage received;
-		if (!completedTransmission){
+	CommPackage received;
+	if (!completedTransmission){
 			received=usartReceivedQueue()->tail->package;
 			*((uint8_t*)received.package+marker)=usartReceive();
 			marker++;
-			if(marker>received.size)
+			if(marker==received.size)
 				completedTransmission=true;
-		}
-		else {
+	}
+	else {
 			received.size=usartReceive();
 			received.dynamic=true;
-			received.package=malloc(received.size+1);
+			received.package=malloc(received.size);
 			queue(usartReceivedQueue(),received);
 			marker=0;
-		}
-
 	}
+}
+/*/
+ISR(USART_RX_vect){
+    uint8_t data=usartReceive();
+    usartTransmit(data);
 
 }
-
-void usartSendText(const __memx char* text,bool dynamic){
-	CommPackage newPackage={(const __memx uint8_t*)text,sizeof(text),dynamic};
+//*/
+void usartSendText(const __memx char* text, uint8_t size, bool dynamic){
+	CommPackage newPackage={(const __memx uint8_t*)text,size,dynamic};
 	queue(usartToSendQueue(),newPackage);
-	UCSR0A|=1<<TXC0;
+	USART_TX_vect();
 }
 
 const char* usartGetText(){
 	CommPackage receivedPackage=dequeue(usartReceivedQueue());
 	return (const char*)receivedPackage.package;
+    PORTB^=1<<PB5;
 }
 
 
 int main(void){
 	usartInit(BAUD);
+    DDRB|=1<<PB5;
+    PORTB^=1<<PB5;
+    const char __flash * text=PSTR("Czesc\n");
+    const char* received;
+    uint8_t size=0;
+
+    while(1){
+        usartSendText(text,sizeof("Czesc\n"),false);
+
+        _delay_ms(1000);
+/*        if(!usartReceivedQueue()->isEmpty){
+            received=usartGetText();
+            while(*(received+size))
+                size++;
+            size++;
+            usartSendText(usartGetText(),size,true);
+            size=0;
+            
+           }
+           */
+    }
 
 }
